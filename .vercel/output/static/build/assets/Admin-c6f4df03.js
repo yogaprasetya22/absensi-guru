88361f520355e969127ab2cb51516f6b48584f0a
yoga <yogaagle123z@gmail.com>
2024-04-02 18:48:03 +0700

    w

---
+++ .vercel/output/static/build/assets/Admin-c6f4df03.js
@@ -0,0 +1,1 @@
+import{j as i}from"./app-df35aaf5.js";import{L as s}from"./Layout-af7b5ed3.js";import"./transition-73bc14cb.js";function d({title:o,auth:r,data:e}){return i.jsx(s,{title:o,user:r==null?void 0:r.user,children:i.jsx("h1",{children:"Admin"})})}export{d as default};
---
+++ .vercel/output/static/build/assets/DetailLaporanAbsensi-b8bc8bbe.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-df35aaf5.js";function i(){return a.jsx("div",{children:"DetailLaporanAbsensi"})}export{i as default};
